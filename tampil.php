d4c536a6f01a48d2fce2319072b0f32f65b41336
TaufikNurRahman123 <118150808+TaufikNurRahman123@users.noreply.github.com>
2023-11-30 19:18:28 +0700

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+        <body>
+            <h3> Tampil Form PHP</h3>
+            
+            Nama <?php echo $_POST["nama"];?><br>
+            Email <?php echo $_POST["email"];?><br>
+            Jenis Kelamin <?php echo $_POST["gender"];?><br>
+        
+       </body>
+</html>
